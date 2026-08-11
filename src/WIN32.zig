6cc4633683b1158d46e33f5013aaa9d8e243feb7
josh <jpwolfe1981@gmail.com>
2026-08-11 04:36:41 -0400

    feat: added stub for win32 bindings

---
+++ src/WIN32.zig
@@ -0,0 +1,30 @@
+// Zig's translate-c breaks certain long macro chains, especially
+// in the context of WIN32.
+//
+// In that context, certain values depend on obfuscated macros that chain together, 
+// eventually going through MinGW.
+//
+// This file serves as lightweight bindings for the WIN32 API. As issues are encountered,
+// this file will grow. Once the Windows implementation is complete, it would be smart
+// to transfer all types, structures, and functions that are used to this file. This way,
+// a user does not need to translate 100k+ lines of C to Zig, which *severely* slows down zls.
+
+
+// WinUser.h
+
+pub const IDC_ARROW           = @as(?[*:0]const u16, @ptrFromInt(32512));
+pub const IDC_IBEAM           = @as(?[*:0]const u16, @ptrFromInt(32513));
+pub const IDC_WAIT            = @as(?[*:0]const u16, @ptrFromInt(32514));
+pub const IDC_CROSS           = @as(?[*:0]const u16, @ptrFromInt(32515));
+pub const IDC_UPARROW         = @as(?[*:0]const u16, @ptrFromInt(32516));
+pub const IDC_SIZE            = @as(?[*:0]const u16, @ptrFromInt(32640));
+pub const IDC_ICON            = @as(?[*:0]const u16, @ptrFromInt(32641));
+pub const IDC_SIZENWSE        = @as(?[*:0]const u16, @ptrFromInt(32642));
+pub const IDC_SIZENESW        = @as(?[*:0]const u16, @ptrFromInt(32643));
+pub const IDC_SIZEWE          = @as(?[*:0]const u16, @ptrFromInt(32644));
+pub const IDC_SIZENS          = @as(?[*:0]const u16, @ptrFromInt(32645));
+pub const IDC_SIZEALL         = @as(?[*:0]const u16, @ptrFromInt(32646));
+pub const IDC_NO              = @as(?[*:0]const u16, @ptrFromInt(32648));
+pub const IDC_HAND            = @as(?[*:0]const u16, @ptrFromInt(32649));
+pub const IDC_APPSTARTING     = @as(?[*:0]const u16, @ptrFromInt(32650));
+pub const IDC_HELP            = @as(?[*:0]const u16, @ptrFromInt(32651));
