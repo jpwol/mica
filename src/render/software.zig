ea0d57422c7a4143862f92d02557322631d1d5d6
josh <jpwolfe1981@gmail.com>
2026-08-12 01:39:16 -0400

    feat: added bresenham's line drawing algorithm

--- src/render/software.zig
+++ src/render/software.zig
@@ -20,6 +20,101 @@ pub fn setPixel(canvas: *Canvas, x: i32, y: i32, color: Color) void {
     canvas.pixels[idx + 3] = color.a;
 }
 
+pub fn drawLine(canvas: *Canvas, x1: i32, y1: i32, x2: i32, y2: i32, color: Color) void {
+    var x: i32 = x1;
+    var y: i32 = y1;
+
+    const dx: i32 = @intCast(@abs(x2 - x1));
+    const sx: i32 = if (x1 < x2) 1 else -1;
+    const dy: i32 = -@as(i32, @intCast(@abs(y2 - y1)));
+    const sy: i32 = if (y1 < y2) 1 else -1;
+    var err =  dx + dy;
+
+    while (true) {
+        setPixel(canvas, x, y, color);
+        const e2 = 2 * err;
+        if (e2 >= dy) {
+            if (x == x2) break;
+            err = err + dy;
+            x = x + sx;
+        }
+        if (e2 <= dx) {
+            if (y == y2) break;
+            err = err + dx;
+            y = y + sy;
+        }
+    }
+}
+
+// Below is Bresenham's four-way octant dispatch line drawing algorithm. It is unused in favor
+// of the unified octant-agnostic version, as performance in this area will be dominated by
+// memory writes instead of a couple extra if calls
+//
+// FIXME: Run some perf tests anyways and remove this if the difference is negligable
+
+// pub fn drawLine(canvas: *Canvas, x1: i32, y1: i32, x2: i32, y2: i32, color: Color) void {
+//     if (@abs(y2 - y1) < @abs(x2 - x1)) {
+//         if (x1 > x2) {
+//             plotLineLow(canvas, x2, y2, x1, y1, color);
+//         } else {
+//             plotLineLow(canvas, x1, y1, x2, y2, color);
+//         }
+//     } else {
+//         if (y1 > y2) {
+//             plotLineHigh(canvas, x2, y2, x1, y1, color);
+//         } else {
+//             plotLineHigh(canvas, x1, y1, x2, y2, color);
+//         }
+//     }
+// }
+//
+//
+// fn plotLineLow(canvas: *Canvas, x1: i32, y1: i32, x2: i32, y2: i32, color: Color) void {
+//     const dx = x2 - x1;
+//     var dy = y2 - y1;
+//     var yi: i32 = 1;
+//     if (dy < 0) {
+//         yi = -1;
+//         dy = -dy;
+//     }
+//     var D = (2 * dy) - dx;
+//     var y = y1;
+//
+//     var x: i32 = x1;
+//     while (x < x2) : (x += 1) {
+//         setPixel(canvas, x, y, color);
+//         if (D > 0) {
+//             y = y + yi;
+//             D = D + (2 * (dy - dx));
+//         } else {
+//             D = D + 2 * dy;
+//         }
+//     }
+// }
+//
+// fn plotLineHigh(canvas: *Canvas, x1: i32, y1: i32, x2: i32, y2: i32, color: Color) void {
+//     var dx = x2 - x1;
+//     const dy = y2 - y1;
+//     var xi: i32 = 1;
+//     if (dx < 0) {
+//         xi = -1;
+//         dx = -dx;
+//     }
+//     var D = (2 * dx) - dy;
+//     var x = x1;
+//
+//     var y: i32 = y1;
+//     while (y < y2) : (y += 1) {
+//         setPixel(canvas, x, y, color);
+//         if (D > 0) {
+//             x = x + xi;
+//             D = D + (2 * (dx - dy));
+//         } else {
+//             D = D + 2 * dx;
+//         }
+//     }
+// }
+
 pub fn fillRect(canvas: *Canvas, x: i32, y: i32, w: u32, h: u32, color: Color) void {
     var row: u32 = 0;
     while (row < h) : (row += 1) {
